eefe5eadead57e37ccd997743989b39a0b6e360a
mango <esudatcom@gmail.com>
2024-01-13 14:12:16 +0900

    @xxToOne 시리즈에 fetch=FetchType.LAZY적용 (필수)

--- src/main/java/jpabook/jpashop/domain/Order.java
+++ src/main/java/jpabook/jpashop/domain/Order.java
@@ -18,14 +18,14 @@ public class Order {
     @Column(name = "order_id")
     private Long id ;
 
-    @ManyToOne
+    @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
 
     @OneToMany(mappedBy = "order")
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne
+    @OneToOne(fetch = FetchType.LAZY)
     @JoinColumn(name="delivery_id") // 연관관계주인공
     private Delivery delivery;
 
